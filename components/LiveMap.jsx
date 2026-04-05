04702bc8cd17b243ad1341b919ed33c7800d1a48
Themugo <mugo.james27@gmail.com>
2026-04-05 21:14:25 +0300

    Update LiveMap.jsx

--- components/LiveMap.jsx
+++ components/LiveMap.jsx
@@ -1,58 +1,20 @@
 "use client";
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet";
-import { useEffect, useState } from "react";
-import "leaflet/dist/leaflet.css";
-
-// Fix default marker icon issue
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-});
 
 export default function LiveMap({ initialDevices = [] }) {
-  const [devices, setDevices] = useState(initialDevices);
-
-  useEffect(() => {
-    const interval = setInterval(async () => {
-      // Replace this with your real API endpoint
-      const updatedDevices = await fetch("/api/devices/live")
-        .then((res) => res.json())
-        .catch(() => devices);
-
-      setDevices(updatedDevices);
-    }, 5000);
-
-    return () => clearInterval(interval);
-  }, [devices]);
-
-  const defaultCenter = [0.0, 36.8219]; // Nairobi
-  const mapCenter =
-    devices.length > 0
-      ? [devices[0].latitude || 0, devices[0].longitude || 36.8219]
-      : defaultCenter;
-
   return (
-    <MapContainer center={mapCenter} zoom={10} className="h-full w-full">
+    <MapContainer center={[0, 0]} zoom={2} className="h-full w-full">
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; OpenStreetMap contributors"
       />
-
-      {devices.map((device) => (
+      {initialDevices.map((device, i) => (
         <Marker
-          key={device.id}
-          position={[device.latitude || 0, device.longitude || 36.8219]}
+          key={i}
+          position={[device.lat ?? 0, device.lng ?? 0]}
         >
-          <Popup>
-            <strong>{device.name}</strong>
-            <br />
-            SIM: {device.simNumber || "-"}
-          </Popup>
+          <Popup>{device.name ?? "Unknown Device"}</Popup>
         </Marker>
       ))}
     </MapContainer>
